8a91cae9cdc36cfab03717be5dff015ccd8630f1
kevinz-08 <189859981+kevinz-08@users.noreply.github.com>
2024-12-03 08:19:47 -0500

    tipos de variables y datos

---
+++ main.py
@@ -0,0 +1,28 @@
+# VARIABLES
+# TIPOS DE DATOS PRIMITIVOS
+# - String = cadenas de caractereres 'str'
+# -Char = Caracter 'str'
+# -Number = Enteros 'int', Decimal 'float'
+# -boolean = Datos logicos 'bool'
+
+name = "Kevin Gutierrez"
+age = 16
+height = 1.70
+camper =  True
+
+# Conjunto de Datos
+# -Array = Conjuntos de datos de una dimension 'list'
+hobbies = list()
+hobbies = ["Cocinar","Programar","Dormir","Jugar Basket"]
+
+# MODIFICAR UN DATO DE UNA LISTA
+# hobbies[2] = "Despertarme"
+
+# SOLICITAR DATOS EN LA CONSOLA Y GUARDARLOS EN LA LISTA
+# dato = input("ingrese otros pasatiempos: ")
+# hobbies.append(dato)
+#print(hobbies)
+
+#ELIMINAR EL ULTIMO DATO DE LA LISTA
+hobbies.pop()
+print(hobbies)
